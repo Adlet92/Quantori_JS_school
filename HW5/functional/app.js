0e82f8f7ab92671ec1f533584f9e986645b2b945
Adlet Baiteliyev <bah_92adlet@mail.ru>
2023-04-18 18:56:53 +0600

    update

--- HW5/functional/app.js
+++ HW5/functional/app.js
@@ -1,5 +1,3 @@
-// showtask();
-// showTaskComleted();
 
 const popupToggle = document.getElementById('myBtn');
   
@@ -32,26 +30,6 @@ popupToggle.addEventListener("click", function(){
   createModal();
 });
 
-// function addItem(){
-//   const taskTitleInput = document.querySelector('input');
-//   addTaskInputVal = taskTitleInput.value;
-//   if (addTaskInputVal.trim() !== 0){
-//     let webtask = localStorage.getItem("localtask");
-//     if(webtask === null){
-//     taskObj = [];
-//     }
-//     else {
-//     taskObj = JSON.parse(webtask);
-//     }
-//     taskObj.push(addTaskInputVal);
-//     localStorage.setItem("localtask", JSON.stringify(taskObj));
-//     console.log(taskObj)
-//     taskTitleInput.value = '';
-//     }
-//   closeItem();
-//   showtask();
-// }
-
 function closeItem(){
   const popup = document.getElementById('mypopup');
   popup.style.display="none";
@@ -64,72 +42,6 @@ function changeTheColorOfButton() {
     document.getElementById("task-button").style.background = "#D3D3D3";
  }
 }
-
-// function showtask(){
-//   let webtask = localStorage.getItem("localtask");
-//   if(webtask == null){
-//     taskObj = [];
-//   }
-//   else {
-//     taskObj = JSON.parse(webtask);
-//   }
-//   let html = '<h3>All Tasks</h3>';
-//   let addTaskList = document.getElementById('ntc');
-//   taskObj.forEach((element, index) => {
-//     html += `
-//             <li><input onclick="checkItem(${index}); deleteItem(${index})" type="checkbox">
-//             <label>${element}</label>
-//             <button class="deleteIcon" onclick="deleteItem(${index})"><img src = "./img/Shape.svg"/></button>
-//             </li>`;
-//   });
-//   addTaskList.innerHTML = html;
-// }
-
-// function showTaskComleted(){
-//   let taskCompleted = localStorage.getItem("completedtask");
-//   if(taskCompleted === null){
-//     taskCmpl = [];
-//   }
-//   else {
-//     taskCmpl = JSON.parse(taskCompleted);
-//   }
-
-//   let html = `<h3>Completed Tasks</h3>`;
-//   let addTaskList = document.getElementById('cmplt');
-//   taskCmpl.forEach((el) => {
-//     html += `
-//             <li><input type="checkbox" checked disabled="disabled">
-//             <label class="compl">${el}</label>
-//             </li>`;
-//   });
-//   addTaskList.innerHTML = html;
-// }
-
-// function deleteItem(index){
-//   let webtask = localStorage.getItem("localtask");
-//   let taskObj = JSON.parse(webtask);
-//   taskObj.splice(index, 1);
-//   localStorage.setItem("localtask", JSON.stringify(taskObj));
-//   showtask();
-// }
-
-// function checkItem(index){
-//   let webtask = localStorage.getItem("localtask");
-//   taskObj = JSON.parse(webtask);
-
-//   let taskCompleted = localStorage.getItem("completedtask");
-//   if(taskCompleted === null){
-//     taskCmpl = [];
-//   }
-//   else {
-//     taskCmpl = JSON.parse(taskCompleted);
-//   }
-//   taskCmpl.push(taskObj[index]);
-
-//   localStorage.setItem("completedtask", JSON.stringify(taskCmpl));
-
-//   showTaskComleted();
-// }
 
 let searchTextBox = document.getElementById("find");
 
@@ -167,7 +79,7 @@ function todoToHTML({id, completed, title}){
   const todoList = document.getElementById('ntc');
 
   todoList.insertAdjacentHTML('beforeend', `
-    <li><input id="check" onclick="toggleCompletedTodo(${id}, ${title})" type="checkbox" ${completed && 'checked'}>
+    <li><input id="check" onclick="toggleCompletedTodo(${id}, '${title}', ${!completed})" type="checkbox" ${completed && 'checked'}>
     <label id="label-title">${title}</label>
     <button class="deleteIcon" onclick="deleteTodo(${id})"><img src = "./img/Shape.svg"/></button>
     </li>
@@ -197,7 +109,6 @@ async function addItem(){
     });
 
     const todo = await res.json();
-    // console.log(todo);
     todoToHTML(todo);
   }
 }
@@ -214,32 +125,15 @@ async function deleteTodo(id){
     document.getElementById('ntc').remove();
   }
 }
-async function toggleCompletedTodo(id, title){
-  console.log("yes")
-  // const completed = document.getElementById('check').checked;
+async function toggleCompletedTodo(id, title, completed){
 
   const res = await fetch(`http://localhost:3000/tasks/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({title, completed:true})
+    body: JSON.stringify({title, completed})
   })
   const data = await res.json();
+  // return data;
 }
-// let searchTextBox = document.getElementById("find");
-
-// searchTextBox.addEventListener("input", function(){
-//   let trlist = document.querySelectorAll("li");
-//   Array.from(trlist).forEach(function(item){
-//     let searchedText = item.innerText;
-//     let searchTextBoxVal = searchTextBox.value;
-//     let re = new RegExp(searchTextBoxVal, 'gi');
-//     if (searchedText.match(re)){
-//       item.style.display="block";
-//     }
-//     else {
-//       item.style.display="none";
-//     }
-//   })
-// });
