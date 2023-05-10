dbdae027f2ce8be55f6185d06fd463794088988d
Adlet Baiteliyev <bah_92adlet@mail.ru>
2023-05-10 13:16:33 +0600

    categories were commented

--- to-do-list/src/components/PostForm/CategoryOptions.tsx
+++ to-do-list/src/components/PostForm/CategoryOptions.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 const CategoryOptions = () => {
   return (
       <div className="option">
-        <div className="option health">health</div>
+        {/* <div className="option health">health</div>
         <div className="option work">work</div>
         <div className="option home">home</div>
-        <div className="option other">other</div>
+        <div className="option other">other</div> */}
       </div>
   );
 };
--- to-do-list/src/components/Weather/Weather.tsx
+++ to-do-list/src/components/Weather/Weather.tsx
@@ -68,3 +68,4 @@ const Weather = () => {
 };
 
 export default Weather;
+
